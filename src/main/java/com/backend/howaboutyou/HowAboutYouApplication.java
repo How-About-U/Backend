e1dae6b55394c95890d78e2488d7465a7a53c79a
thingk0 <audtjd9080@naver.com>
2023-02-22 15:13:23 +0900

    HowAboutYou - Backend Prj Settings

---
+++ src/main/java/com/backend/howaboutyou/HowAboutYouApplication.java
@@ -0,0 +1,13 @@
+package com.backend.howaboutyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowAboutYouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowAboutYouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/howaboutyou/HowAboutYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.howaboutyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowAboutYouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
