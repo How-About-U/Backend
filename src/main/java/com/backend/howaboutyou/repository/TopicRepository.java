88cf58ea117a5b5318edcf6a8e54c367c3b0e284
thingk0 <audtjd9080@naver.com>
2023-03-05 18:03:35 +0900

    feat: Find topics by date in TopicRepository.

--- src/main/java/com/backend/howaboutyou/repository/TopicRepository.java
+++ src/main/java/com/backend/howaboutyou/repository/TopicRepository.java
@@ -1,8 +1,14 @@
 package com.backend.howaboutyou.repository;
 
-import com.backend.howaboutyou.entity.Opin;
-import com.backend.howaboutyou.entity.Topic;
+import com.backend.howaboutyou.domain.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 public interface TopicRepository extends JpaRepository<Topic, Long> {
+
+    Optional<Topic> findTopicByTopicDate(LocalDate topicDate);
+
+    boolean existsByTopicDate(LocalDate topicDate);
 }
