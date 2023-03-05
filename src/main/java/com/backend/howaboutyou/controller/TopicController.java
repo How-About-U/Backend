f44d088cae98f2ef6a7ed6a7405e2055cdad1f64
thingk0 <audtjd9080@naver.com>
2023-03-05 18:12:52 +0900

    feat: Implementing the Topic API Controller.
    Controller for using logic implemented by topic service in API format.

---
+++ src/main/java/com/backend/howaboutyou/controller/TopicController.java
@@ -0,0 +1,30 @@
+package com.backend.howaboutyou.controller;
+
+import com.backend.howaboutyou.dto.TopicRequestDto;
+import com.backend.howaboutyou.service.TopicService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/topic")
+public class TopicController {
+
+    private final TopicService topicService;
+
+    @PostMapping("/save")
+    public ResponseEntity<?> saveTopic(@RequestBody TopicRequestDto topicRequestDto) {
+        log.info("TopicController.saveTopic()");
+        return ResponseEntity.ok().body(topicService.saveTopic(topicRequestDto));
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<?> getTopic() {
+        log.info("TopicController.getTopic()");
+        return ResponseEntity.ok().body(topicService.getTodayTopic());
+    }
+
+}
