d3643e2dbadcac65517c7638aa0885b1645f008c
thingk0 <audtjd9080@naver.com>
2023-03-08 20:01:20 +0900

    feat: Added API to store entity 'Opin'.

--- src/main/java/com/backend/howaboutyou/controller/OpinController.java
+++ src/main/java/com/backend/howaboutyou/controller/OpinController.java
@@ -1,14 +1,12 @@
 package com.backend.howaboutyou.controller;
 
+import com.backend.howaboutyou.dto.opin.OpinRequestDto;
 import com.backend.howaboutyou.service.OpinService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -17,6 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class OpinController {
 
     private final OpinService opinService;
+
+    @PostMapping("/save")
+    public ResponseEntity<?> opinSave(@RequestBody OpinRequestDto requestDto) {
+        return ResponseEntity.ok().body(opinService.save(requestDto));
+    }
 
     @GetMapping("/get")
     public ResponseEntity<?> opinSearch(@Nullable @RequestBody Boolean searchCond) {
