4f4e70e8803effb38c22a25d9dfadd07daad12c3
thingk0 <audtjd9080@naver.com>
2023-03-10 19:17:08 +0900

    feat: 로그인 응답용 DTO 추가.

---
+++ src/main/java/com/backend/howaboutyou/dto/member/LoginResponseDto.java
@@ -0,0 +1,16 @@
+package com.backend.howaboutyou.dto.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponseDto {
+    private String username;
+    private String email;
+    private String token;
+}
